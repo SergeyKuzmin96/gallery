3d76d4f5c043b23e7ba8931e3d2b305be16ac5f4
SergeyKuzmin96 <kuzminsergeyanatolyevich33@gmail.com>
2022-04-11 14:36:59 +0700

    add to views template/head.php

--- views/signinView.php
+++ views/signinView.php
@@ -9,27 +9,7 @@ if ($_SESSION['user']) {
 
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Badge Signup Form Flat Responsive Widget Template :: w3layouts</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <meta name="keywords"
-          content="Badge Signup Form template Responsive, Login form web template,Flat Pricing tables,Flat Drop downs  Sign up Web Templates, Flat Web Templates, Login sign up Responsive web template, SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design"/>
-    <script type="application/x-javascript"> addEventListener("load", function () {
-            setTimeout(hideURLbar, 0);
-        }, false);
-
-        function hideURLbar() {
-            window.scrollTo(0, 1);
-        } </script>
-    <!-- Custom Theme files -->
-    <link href="../public/css/style.css" rel="stylesheet" type="text/css" media="all"/>
-    <!-- //Custom Theme files -->
-    <!-- web font -->
-    <link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
-    <!--web font-->
-    <!-- //web font -->
-</head>
+<?php require_once 'template/head.php' ?>
 <body>
 <!-- main -->
 <div class="w3layouts-main">
@@ -64,12 +44,6 @@ if ($_SESSION['user']) {
         ?>
     </div>
 </div>
-<!-- //main -->
-<!-- copyright -->
-<div class="copyright">
-    <p>© 2017 Badge Signup Form . All rights reserved | Design by <a href="http://w3layouts.com/" target="_blank">W3layouts</a>
-    </p>
-</div>
-<!-- //copyright -->
+
 </body>
 </html>
